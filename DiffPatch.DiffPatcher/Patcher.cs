7446fc3f2c6d0a20cbbc0ab2f2c358160a8eb47c
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-25 01:23:45 +0200

    Implémentation méthode Patch (Work In Progess)

--- DiffPatch.DiffPatcher/Patcher.cs
+++ DiffPatch.DiffPatcher/Patcher.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DiffPatch.Core;
 using DiffPatch.Data;
 
 namespace DiffPatch.DiffPatcher
@@ -8,8 +12,38 @@ namespace DiffPatch.DiffPatcher
     {
         public static String Patch(String src, IEnumerable<ChunkDiff> chunkDiffs)
         {
-            //TODO
-            return null;
+            IEnumerable<String> srcLines = StringHelper.SplitLines(src);
+            int lineIndex = 1;
+
+
+            IEnumerable<Tuple<Int32, Int32, ChunkDiff>> chunkDiffByOldStart = chunkDiffs.Select(d => new Tuple<int, int, ChunkDiff>(d.OldStart, d.OldStart + d.OldLines, d));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            foreach (string srcLine in srcLines)
+            {
+                Tuple<int, int, ChunkDiff> tuple = chunkDiffByOldStart.FirstOrDefault(kvp => lineIndex >= kvp.Item1 && lineIndex < kvp.Item2);
+                if (tuple == null)
+                {
+                    stringBuilder.AppendLine(srcLine);
+                }
+                else
+                {
+                    LineDiff lineDiff = tuple.Item3.Changes.FirstOrDefault(l => tuple.Item1 + l.OldIndex == lineIndex);
+                    if (lineDiff != null)
+                    {
+                        if (lineDiff.Normal || lineDiff.Add)
+                        {
+                            stringBuilder.AppendLine(lineDiff.Content);
+                        }
+                    }
+
+                }
+                lineIndex++;
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
