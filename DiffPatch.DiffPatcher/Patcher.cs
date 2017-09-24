68c4e7bfb2941b954149390143da25b9e1c1dda8
Amael BERTEAU <amael.berteau@tech-easy.fr>
2017-09-25 00:06:04 +0200

    Ajout du projet DiffPatch.DiffPatcher

---
+++ DiffPatch.DiffPatcher/Patcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using DiffPatch.Data;
+
+namespace DiffPatch.DiffPatcher
+{
+    public class Patcher
+    {
+        public static String Patch(String src, IEnumerable<ChunkDiff> chunkDiffs)
+        {
+            //TODO
+            return null;
+        }
+    }
+}
